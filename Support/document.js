a05c7c769125c16d19a16c9cd568b02de85a0ecb
Nate Silva <nate@desi.com>
2013-11-14 08:41:55 -0800

    (try to) close the report window on ESCape

--- Support/document.js
+++ Support/document.js
@@ -1,6 +1,17 @@
-/* global Zepto, document, EJS, context, ejsTemplate */
+/* global Zepto, document, EJS, context, ejsTemplate, TextMate */
 
 Zepto(document).ready(function($) {
+  // (try to) close the report window when the user presses ESCape
+  $(document).keydown(function(e) {
+    if (e.keyCode === 27) {
+      e.preventDefault();
+      var cmd = '"tell application \\"System Events\\" ' +
+        'to keystroke \\"w\\" using command down"';
+      TextMate.system('osascript -e ' + cmd, function(){});
+    }
+  });
+
+  // render the template and inject it into the page
   var html = new EJS({text: ejsTemplate}).render(context);
   $('#content').html(html);
 });
