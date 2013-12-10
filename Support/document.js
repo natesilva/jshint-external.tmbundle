9dde6ee9a28391eb8c70eadbc4df7b4783192499
Nate Silva <nate@desi.com>
2013-12-10 13:23:50 -0800

    provide links to error/warning explanations
    Also, open external links in a browser instead of in the TextMate results window.

--- Support/document.js
+++ Support/document.js
@@ -1,4 +1,4 @@
-/* global Zepto, document, EJS, context, ejsTemplate, TextMate */
+/* global Zepto, document, EJS, context, ejsTemplate, TextMate, error_explanations */
 
 Zepto(document).ready(function($) {
   // (try to) close the report window when the user presses ESCape
@@ -14,4 +14,26 @@ Zepto(document).ready(function($) {
   // render the template and inject it into the page
   var html = new EJS({text: ejsTemplate}).render(context);
   $('#content').html(html);
+
+  // add links to error/warning codes, when available
+  $('.issue-code').each(function(index, item) {
+    var issueCode = $(item).text();
+    if (error_explanations.hasOwnProperty(issueCode)) {
+      var url = error_explanations[issueCode];
+      $(item).wrap('<a href="' + url + '" class="open-external">');
+    }
+  });
+
+  // By default, links will open in the TextMate results window. If
+  // the <a> tag has class "open-external" we’ll catch it and open
+  // the link in the user’s browser instead.
+  $('.open-external').on('click', function(e) {
+    e.preventDefault();
+    var href = $(this).attr('href');
+    if (!href.match(/^http(?:s?)\:\/\/[^\/]/)) {
+      // doesn’t look like a normal URL
+      return;
+    }
+    TextMate.system('open "' + encodeURI(href) + '"', null);
+  });
 });
