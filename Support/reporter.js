2790504f918e7511660d3a93727a11870e2bdeda
Nate Silva <nate@desi.com>
2013-12-10 16:03:56 -0800

    sort the results by line and character

--- Support/reporter.js
+++ Support/reporter.js
@@ -5,6 +5,14 @@
 module.exports = {
   reporter: function(res) {
     var report = res.map(function(r) { return r.error; });
+    report.sort(function(a, b) {
+      if (a.line === b.line && a.character === b.character) { return 0; }
+
+      if (a.line < b.line) { return -1; }
+      if (a.line === b.line && a.character < b.character) { return -1; }
+
+      return 1;
+    });
     process.stdout.write(JSON.stringify(report) + '\n');
   }
 };
