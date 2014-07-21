03f13863066c47dfe80af512add3cd867b264618
Nate Silva <nate@natesilva.com>
2014-07-21 12:04:25 -0700

    revert to manually managing the output window

--- Support/document.js
+++ Support/document.js
@@ -1,8 +1,18 @@
-/* global Zepto, document, EJS, context, ejsTemplate, TextMate */
+/* global Zepto, document, EJS, context, ejsTemplate, TextMate, window */
 /* global error_explanations */
 
 Zepto(document).ready(function($) {
   var VERSION = '1.1.0';
+
+  // Remove the marker flag indicating that the validation window
+  // for this document is showing.
+  $(document).on('visibilitychange', function() {
+    window.setTimeout(function() {
+      if (document.hidden && ('markerFile' in context) && context.markerFile) {
+        TextMate.system('/bin/rm "' + context.markerFile + '"');
+      }
+    }, 1);
+  });
 
   // close the report window when the user presses ESCape
   $(document).keydown(function(e) {
